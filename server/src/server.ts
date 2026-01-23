04c2ab9f4b4b851f9f3979ca33bdbf1d56371d69
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-01-23 12:41:52 -0600

    fix express route error

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,7 +25,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-  app.get('*', (_req, res) => {
+  app.get('/*', (_req, res) => {
     res.sendFile(
       path.join(__dirname, '../../client/dist/index.html')
     );
