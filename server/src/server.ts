0f52702e6c1dfc40facaccb605ff8b4e1c738f8f
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-01-23 12:52:50 -0600

    register server routes before connect to client

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,6 @@ import { sequelize } from './models/index.js';
 
 dotenv.config();
 
-// instantiate the server.
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,23 +15,19 @@ app.get('/', (_req, res) => {
   res.send('Server is running 🚀');
 });
 
+app.use(routes);
+
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
-
   app.get('/*', (_req, res) => {
-    res.sendFile(
-      path.join(__dirname, '../../client/dist/index.html')
-    );
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
-
-app.use(routes);
 
 sequelize.sync({ alter: true }).then(() => {
   app.listen(PORT, () => {
